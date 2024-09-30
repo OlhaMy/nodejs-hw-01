e09f118584cf124e34da47e1a86a47c690360019
Olha Mykhalchuk <124881449+OlhaMy@users.noreply.github.com>
2024-09-30 17:44:31 +0200

    summary write code

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,13 @@
-const generateContacts = async (number) => {};
+import { readContacts } from '../utils/readContacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const generateContacts = async (number) => {
+  const contactList = await readContacts();
+  const newContact = Array(number)
+    .fill(0)
+    .map(() => createFakeContact());
+  await writeContacts([...contactList, ...newContact]);
+};
 
 generateContacts(5);
